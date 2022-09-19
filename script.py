79d733e92f6b0729cb52d077ddcfaf5a27f2d823
EarthlyEric6 <minecaeft.creeper.eric@gmail.com>
2022-09-19 18:51:28 +0800

    better cli

--- script.py
+++ script.py
@@ -1,10 +1,28 @@
+import os
 import asyncio
-import datetime
 import genshin
-from discord_webhook import AsyncDiscordWebhook, DiscordEmbed
+import platform
+from colored import fg, attr
+from discord_webhook import DiscordWebhook, DiscordEmbed
 from configobj import ConfigObj
 
 class config():
+    LOGO="""
+    
+    ░██████╗░███████╗███╗░░██╗░██████╗██╗░░██╗██╗███╗░░██╗  ██╗███╗░░░███╗██████╗░░█████╗░░█████╗░████████╗
+    ██╔════╝░██╔════╝████╗░██║██╔════╝██║░░██║██║████╗░██║  ██║████╗░████║██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝
+    ██║░░██╗░█████╗░░██╔██╗██║╚█████╗░███████║██║██╔██╗██║  ██║██╔████╔██║██████╔╝███████║██║░░╚═╝░░░██║░░░
+    ██║░░╚██╗██╔══╝░░██║╚████║░╚═══██╗██╔══██║██║██║╚████║  ██║██║╚██╔╝██║██╔═══╝░██╔══██║██║░░██╗░░░██║░░░
+    ╚██████╔╝███████╗██║░╚███║██████╔╝██║░░██║██║██║░╚███║  ██║██║░╚═╝░██║██║░░░░░██║░░██║╚█████╔╝░░░██║░░░
+    ░╚═════╝░╚══════╝╚═╝░░╚══╝╚═════╝░╚═╝░░╚═╝╚═╝╚═╝░░╚══╝  ╚═╝╚═╝░░░░░╚═╝╚═╝░░░░░╚═╝░░╚═╝░╚════╝░░░░╚═╝░░░
+
+    ░█████╗░██╗░░░██╗████████╗░█████╗░  ░██████╗██╗░██████╗░███╗░░██╗░░░░░░██╗███╗░░██╗
+    ██╔══██╗██║░░░██║╚══██╔══╝██╔══██╗  ██╔════╝██║██╔════╝░████╗░██║░░░░░░██║████╗░██║
+    ███████║██║░░░██║░░░██║░░░██║░░██║  ╚█████╗░██║██║░░██╗░██╔██╗██║█████╗██║██╔██╗██║
+    ██╔══██║██║░░░██║░░░██║░░░██║░░██║  ░╚═══██╗██║██║░░╚██╗██║╚████║╚════╝██║██║╚████║
+    ██║░░██║╚██████╔╝░░░██║░░░╚█████╔╝  ██████╔╝██║╚██████╔╝██║░╚███║░░░░░░██║██║░╚███║
+    ╚═╝░░╚═╝░╚═════╝░░░░╚═╝░░░░╚════╝░  ╚═════╝░╚═╝░╚═════╝░╚═╝░░╚══╝░░░░░░╚═╝╚═╝░░╚══╝
+    """
     config=ConfigObj('config.ini')
     # 身分驗證
     auth=config['Authentication']
@@ -20,30 +38,47 @@ class config():
         discord_enable=False
     discord_webhook_url=discord_webhook['webhook_url']
 
+def webhook(AlreadyClaimed:bool,reward=None):
+    webhook=DiscordWebhook(url=config.discord_webhook_url)
+    if AlreadyClaimed:
+        emoji=':x:'
+    else:
+        emoji=':white_check_mark:'
+    embed=DiscordEmbed(title=f'{emoji} |原神|每日簽到')
+    embed.set_color(color='03b2f8')
+    embed.set_timestamp()
+    if AlreadyClaimed:
+        embed.add_embed_field(name='今日每日獎勵已被搶先領取!',value='`你可能已經使用網頁簽到過了。`')
+    else:
+        embed.set_thumbnail(url=reward.icon)
+        embed.add_embed_field(name='今日領取物品',value='`%s 個 %s`'%(str(reward.amount),reward.name))
+    embed.set_footer(text='Genshin-Impact-Sign-in')
+
+    webhook.add_embed(embed)
+
+    webhook.execute()
+
 async def main():
     client=genshin.Client(
         game=genshin.Game.GENSHIN
     )
     client.set_cookies(ltuid=config.ltuid,ltoken=config.ltoken)
     try:
-        reward=await client.claim_daily_reward(lang='zh-tw')    
+        webhook(AlreadyClaimed=True)
+        reward=await client.claim_daily_reward(lang='zh-tw')
     except genshin.AlreadyClaimed:
-        print('今日每日獎勵已領取!')
+        print('執行結果:')
+        print('%s今日每日獎勵已被搶先領取! %s'%(fg(3),attr(0)))
     else:
+        webhook(reward=reward,AlreadyClaimed=False)
         print('今日已領取 %s 個 %s'%(str(reward.amount),reward.name))
-    if config.discord_enable:
-        webhook=AsyncDiscordWebhook(url=config.discord_webhook_url)
-        embed=DiscordEmbed(title=':white_check_mark: |原神|每日簽到')
-        embed.set_color(color='03b2f8')
-        embed.set_thumbnail(url=reward.icon)
-        embed.set_timestamp(timestamp=datetime.now())
-        embed.add_embed_field(name='今日領取物品',value='`%s 個 %s`'%(str(reward.amount),reward.name))
-        embed.set_footer('Genshin-Impact-Sign-in')
+    
 
-        webhook.add_embed(embed)
-
-        await webhook.execute()
-
+os.system('cls' if os.name=='nt' else 'clear')
+print(config.LOGO)
+print('==================================================================================================================')
+if platform.system()=='Windows':
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())        
 asyncio.run(main())
     
     
